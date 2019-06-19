9b83a475c58b2cfc1df87edaa019f93319869110
Mateus <mateus_sat@hotmail.com>
2019-06-18 22:30:09 -0300

    iniciado os componentes

--- app/src/main/java/com/example/calcular_salario/MainActivity.java
+++ app/src/main/java/com/example/calcular_salario/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.calcular_salario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtSalBruto, editDep, editDescINSS, edtAliIRPF, edtBaseINSS, edtBaseIRPF, edtValINSS, edtValIRPF, edtDedu, edtSalLiqui;
+    Button btnGraf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

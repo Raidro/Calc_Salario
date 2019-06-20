67e8b77809c06f11aafd64f6aa77c7c0c5c4de52
Mateus <mateus_sat@hotmail.com>
2019-06-19 22:07:56 -0300

    criado o private myChart

--- app/src/main/java/com/example/calcular_salario/GraficoActivity.java
+++ app/src/main/java/com/example/calcular_salario/GraficoActivity.java
@@ -4,7 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraficoActivity extends AppCompatActivity {
+
+    private PieChart myChart;
+    List<PieEntry> valores = new ArrayList<>();
+    PieDataSet dataSet1;
+    PieData dados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

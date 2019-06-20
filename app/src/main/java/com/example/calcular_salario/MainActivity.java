7a6bfe0eb9dd0a941d31a3ad12078372a4fc79bc
Mateus <mateus_sat@hotmail.com>
2019-06-20 00:31:34 -0300

    retirada dos cifrões pq eu sou burro

--- app/src/main/java/com/example/calcular_salario/MainActivity.java
+++ app/src/main/java/com/example/calcular_salario/MainActivity.java
@@ -117,13 +117,12 @@ public class MainActivity extends AppCompatActivity {
 
 
         editDescINSS.setText(String.format("%.2f", Aliq * 100) + " %");  //mostra na tela o valor da Aliq
-<<<<<<< HEAD
-        edtBaseINSS.setText("R$ " + String.format("%.2f", BaseInss)); // mostra na tela o valor de BaseInss
-        edtValINSS.setText("R$ " + String.format("%.2f", ValorInss)); // mostra na tela o valor de ValorInss
-=======
-        edtBaseINSS.setText("R$ "+ String.format("%.2f", BaseInss)); // mostra na tela o valor de BaseInss
-        edtValINSS.setText("R$ "+  String.format("%.2f", ValorInss)); // mostra na tela o valor de ValorInss
->>>>>>> master
+
+        edtBaseINSS.setText(String.format("%.2f", BaseInss)); // mostra na tela o valor de BaseInss
+        edtValINSS.setText(String.format("%.2f", ValorInss)); // mostra na tela o valor de ValorInss
+
+        edtBaseINSS.setText(String.format("%.2f", BaseInss)); // mostra na tela o valor de BaseInss
+        edtValINSS.setText(String.format("%.2f", ValorInss)); // mostra na tela o valor de ValorInss
 
 
     }
@@ -158,17 +157,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         edtAliIRPF.setText(String.format("%.3f", AliqIRPF * 100) + " %");
-<<<<<<< HEAD
-        edtBaseIRPF.setText("R$ " + String.format("%.2f", BaseIrpf));
-        edtValIRPF.setText("R$ " + String.format("%.2f", ValorIRPF));
-        edtDedu.setText("R$ " + String.format("%.2f", deduc));
-        edtSalLiqui.setText("R$ " + String.format("%.2f", SalaLiqui));
-=======
-        edtBaseIRPF.setText("R$ "+ String.format("%.2f", BaseIrpf));
-        edtValIRPF.setText("R$ "+ String.format("%.2f", ValorIRPF));
-        edtDedu.setText("R$ "+ String.format("%.2f", deduc));
-        edtSalLiqui.setText("R$ "+ String.format("%.2f", SalaLiqui));
->>>>>>> master
+
+        edtBaseIRPF.setText(String.format("%.2f", BaseIrpf));
+        edtValIRPF.setText(String.format("%.2f", ValorIRPF));
+        edtDedu.setText(String.format("%.2f", deduc));
+        edtSalLiqui.setText(String.format("%.2f", SalaLiqui));
+
+        edtBaseIRPF.setText(String.format("%.2f", BaseIrpf));
+        edtValIRPF.setText(String.format("%.2f", ValorIRPF));
+        edtDedu.setText(String.format("%.2f", deduc));
+        edtSalLiqui.setText(String.format("%.2f", SalaLiqui));
 
 
     }
